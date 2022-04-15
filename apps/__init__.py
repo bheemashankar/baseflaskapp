19f6cd4aeda9d323bb950b93d362518effa3dd36
Bheema <bheema.machani@gmail.com>
2022-04-15 22:08:45 +0530

    changes done

--- apps/__init__.py
+++ apps/__init__.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from apps.middleware import before_my_blueprint
+from apps.middleware import before_web_blueprint, before_api_blueprint
 
 
 app = Flask(__name__)
@@ -9,24 +9,13 @@ app.config.from_object('config')
 
 db = SQLAlchemy(app)
 
-
-@app.errorhandler(404)
-def not_found(error):
-	return render_template('404.html'), 404
-
-
-@app.route('/', strict_slashes=False)
-def root():
-    return redirect(url_for('webapp.dashboard'))
-
-
 from apps.onboard import onboard_app
 from apps.api import api_app
 from apps.web import web_app
 
 app.before_request_funcs = {
-    'apiapp': [before_my_blueprint],
-	'webapp': [before_my_blueprint]
+    'apiapp': [before_api_blueprint],
+	'webapp': [before_web_blueprint]
 }
 
 app.register_blueprint(onboard_app)
@@ -34,3 +23,5 @@ app.register_blueprint(api_app)
 app.register_blueprint(web_app)
 
 db.create_all()
+
+from apps import views
--- apps/middleware.py
+++ apps/middleware.py
@@ -1,6 +1,16 @@
 from flask import redirect, url_for
 
 
-def before_my_blueprint():
+def is_onboard_done():
     if True:
+        return True
+    return False
+
+
+def before_web_blueprint():
+    if is_onboard_done():
         return redirect(url_for('onboard.welcome'))
+
+def before_api_blueprint():
+    if is_onboard_done():
+        return redirect(url_for('index'))
